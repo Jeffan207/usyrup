372db55f453fe1d79cd2240344299c68776e3046
Hasan Demirtaş <utsukushihito@outlook.com>
2025-05-06 19:32:00 +0300

    Update SyrupInjectorOptions.cs

--- SyrupSource/Syrup/Framework/SyrupInjectorOptions.cs
+++ SyrupSource/Syrup/Framework/SyrupInjectorOptions.cs
@@ -1,7 +1,9 @@
-﻿namespace Syrup.Framework {
-    /// <summary>
-    /// Optional options block that can be passed to the SyrupInjector
-    /// </summary>
+﻿using System;
+
+/// <summary>
+/// Optional options block that can be passed to the SyrupInjector
+/// </summary>
+namespace Syrup.Framework {
     public class SyrupInjectorOptions {
 
         public bool VerboseLogging { get; set; }
