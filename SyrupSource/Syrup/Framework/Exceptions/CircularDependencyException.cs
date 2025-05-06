b1a06be9fcae689f64c8cf00bd89e2e73d5b2bef
Hasan Demirtaş <utsukushihito@outlook.com>
2025-05-06 16:30:38 +0300

    dep. exception.

---
+++ SyrupSource/Syrup/Framework/Exceptions/CircularDependencyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Syrup.Framework.Exceptions {
+    public class CircularDependencyException : Exception {
+        public CircularDependencyException() {
+        }
+
+        public CircularDependencyException(string message)
+            : base(message) {
+        }
+
+        public CircularDependencyException(string message, Exception inner)
+            : base(message, inner) {
+        }
+    }
+}
