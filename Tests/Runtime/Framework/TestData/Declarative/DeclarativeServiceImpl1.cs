eea99fd55e914821d1df1079d7d1eff0e57d4ea5
Hasan Demirtaş <utsukushihito@outlook.com>
2025-05-08 11:03:05 +0300

    Update DeclarativeServiceImpl1.cs

--- Tests/Runtime/Framework/TestData/Declarative/DeclarativeServiceImpl1.cs
+++ Tests/Runtime/Framework/TestData/Declarative/DeclarativeServiceImpl1.cs
@@ -5,10 +5,16 @@ namespace Tests.Framework.TestData {
         public string Id { get; } = Guid.NewGuid().ToString();
         private readonly DeclarativeDependency _dependency;
 
-        public DeclarativeServiceImpl1() { }
+        public static bool WasInstantiated_ServiceImpl1 = false;
 
-        public DeclarativeServiceImpl1(DeclarativeDependency dependency) =>
+        public DeclarativeServiceImpl1() {
+            WasInstantiated_ServiceImpl1 = true;
+        }
+
+        public DeclarativeServiceImpl1(DeclarativeDependency dependency) {
             _dependency = dependency;
+            WasInstantiated_ServiceImpl1 = true;
+        }
 
         public string Greet() => $"Hello from ServiceImpl1 ({Id})" +
                                  (_dependency != null ? " with " + _dependency.Describe() : "");
