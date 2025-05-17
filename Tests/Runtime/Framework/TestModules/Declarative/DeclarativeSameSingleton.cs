654bbd3e20a4541f0cc5705b3297fcfd2707e598
Hasan Demirtaş <utsukushihito@outlook.com>
2025-05-17 04:49:25 +0300

    Fix the same singleton binding. (#22)
    Fix binding interface<>concrete singleton bindings and lone concrete singleton bindings not sharing the same singleton instance.

---
+++ Tests/Runtime/Framework/TestModules/Declarative/DeclarativeSameSingleton.cs
@@ -0,0 +1,12 @@
+﻿using Syrup.Framework;
+using Syrup.Framework.Declarative;
+using Tests.Framework.TestData.Declarative;
+
+namespace Tests.Framework.TestModules {
+public class DeclarativeSameSingleton : ISyrupModule {
+    public void Configure(IBinder binder) {
+        binder.Bind<SameSingleton>().AsSingleton();
+        binder.Bind<ISameSingleton>().To<SameSingleton>().AsSingleton();
+    }
+}
+}
